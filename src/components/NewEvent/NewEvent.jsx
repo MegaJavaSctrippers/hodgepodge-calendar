478e2730007f9ee716d728d30126c9ec725481e0
Aigerim Erkebaeva <erkebaeva.aigerim88@gmail.com>
2022-06-20 15:27:56 +0600

    feat: change the implemention of date picker by material ui

--- src/components/NewEvent/NewEvent.jsx
+++ src/components/NewEvent/NewEvent.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import { FormControlLabel, Switch } from '@mui/material'
 import s from './NewEvent.module.css'
 import down from '../../assets/icons/down.svg'
-import Date from './Date/Date'
-import DayOfWeek from './DayOfWeek/DayOfWeek'
+import DateNew from './Date/DateNew'
+// import DayOfWeek from './DayOfWeek/DayOfWeek'
 import close from '../../assets/icons/closeIcon.png'
 import StartTime from './StartTime/StartTime'
 import EndTime from './EndTime/EndTime'
@@ -15,62 +13,60 @@ import Reminder2 from './Reminder2/Reminder2'
 
 function NewEvent() {
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <div className={s.overlay}>
-        <form className={s.new_event}>
-          <div className={s.container}>
-            <p className={s.title}>Новое событие</p>
-            <img className={s.close} src={close} alt="close icon" />
-            <p className={s.label}>Название</p>
-            <textarea className={s.input} />
-            <p className={s.attachment}>Прикрепить файл</p>
-            <div className={s.date_title}>
-              <div>Дата и время</div>
-              <FormControlLabel control={<Switch />} label="Весь день" />
-            </div>
-            <div className={s.date_wrapper}>
-              <div className={s.date_day}>
-                <Date />
-                <DayOfWeek />
-              </div>
-              <StartTime />
-              <EndTime />
-            </div>
-            <div className={s.repeat_wrapper}>
-              <Repeat />
-              <Reminder />
-              <Reminder2 />
-            </div>
-            <div className={s.user_room_wrapper}>
-              <div className={s.user_wrapper}>
-                <p className={s.label}>Участники(1)</p>
-                <div className={s.participants}>Jenny</div>
-              </div>
-              <div className={s.room_wrapper}>
-                <p className={s.label}>Помещение</p>
-                <div className={s.room}>
-                  <img className={s.room_dropdown} src={down} alt="dropdown icon" />
-                </div>
-              </div>
-            </div>
-            <div className={s.calendar_permission}>
-              <div className={s.calendar}>
-                <p className={s.label}>Календарь</p>
-                <div className={s.calendar_item}>Рабочий</div>
-              </div>
-              <div className={s.permission}>
-                <p className={s.label}>Разрешение на доступ к мероприятию</p>
-                <div className={s.permission_item}>Общедоступное</div>
-                <img className={s.down_icon} src={down} alt="dropdown icon" />
-              </div>
-            </div>
-            <button className={s.btn} type="button">
-              Сохранить
-            </button>
+    <div className={s.overlay}>
+      <form className={s.new_event}>
+        <div className={s.container}>
+          <p className={s.title}>Новое событие</p>
+          <img className={s.close} src={close} alt="close icon" />
+          <p className={s.label}>Название</p>
+          <textarea className={s.input} />
+          <p className={s.attachment}>Прикрепить файл</p>
+          <div className={s.date_title}>
+            <div>Дата и время</div>
+            <FormControlLabel control={<Switch />} label="Весь день" />
           </div>
-        </form>
-      </div>
-    </LocalizationProvider>
+          <div className={s.date_wrapper}>
+            {/* <div className={s.date_day}> */}
+            <DateNew />
+            {/* <DayOfWeek /> */}
+            {/* </div> */}
+            <StartTime />
+            <EndTime />
+          </div>
+          <div className={s.repeat_wrapper}>
+            <Repeat />
+            <Reminder />
+            <Reminder2 />
+          </div>
+          <div className={s.user_room_wrapper}>
+            <div className={s.user_wrapper}>
+              <p className={s.label}>Участники(1)</p>
+              <div className={s.participants}>Jenny</div>
+            </div>
+            <div className={s.room_wrapper}>
+              <p className={s.label}>Помещение</p>
+              <div className={s.room}>
+                <img className={s.room_dropdown} src={down} alt="dropdown icon" />
+              </div>
+            </div>
+          </div>
+          <div className={s.calendar_permission}>
+            <div className={s.calendar}>
+              <p className={s.label}>Календарь</p>
+              <div className={s.calendar_item}>Рабочий</div>
+            </div>
+            <div className={s.permission}>
+              <p className={s.label}>Разрешение на доступ к мероприятию</p>
+              <div className={s.permission_item}>Общедоступное</div>
+              <img className={s.down_icon} src={down} alt="dropdown icon" />
+            </div>
+          </div>
+          <button className={s.btn} type="button">
+            Сохранить
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
