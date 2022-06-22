64cfee1347130f70fab92b92c3156fa76c043a1e
Tilek-TrueBoy <tilek.trueboy@mail.ru>
2022-06-22 17:27:37 +0600

    feat(register): add registration to the app

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { reg } from '../../helpers/regexp'
 import classes from './Form.module.css'
 
 function Form() {
@@ -7,22 +8,98 @@ function Form() {
     password: '',
     password2: '',
   })
+  const [emailEmpty, setEmailEmpty] = useState(false)
+  const [passwordEmpty, setPasswordEmpty] = useState(false)
+  const [passwordEmptyRepeat, setPasswordEmptyRepeat] = useState(false)
+  const [validForm, setValidForm] = useState(false)
+  const [emailError, setEmailError] = useState('Email не может быть пустым')
+  const [passError, setPassError] = useState('Пароль не может быть пустым')
+  const [repeatPass, setRepeatPass] = useState('Пароль должен совпадать')
+
+  const changeInputRegister = (e) => {
+    e.persist()
+    setRegister({
+      ...register,
+      [e.target.name]: e.target.value,
+    })
+    if (e.target.name === 'email') {
+      if (!reg.email.test(String(e.target.value).toLowerCase())) {
+        setEmailError('Некорректный емайл')
+      } else {
+        setEmailError('')
+      }
+    }
+    if (e.target.name === 'password') {
+      if (!reg.password.test(String(e.target.value).toLowerCase())) {
+        setPassError(
+          'Пароль должен состоять хотя бы из 6 строк и содержать минимум одну цифру и один символ',
+        )
+      } else {
+        setPassError('')
+      }
+    }
+  }
+
+  const blurHandler = (e) => {
+    // eslint-disable-next-line default-case
+    switch (e.target.name) {
+      case 'email':
+        setEmailEmpty(true)
+        break
+      case 'password':
+        setPasswordEmpty(true)
+        break
+      case 'password2':
+        setPasswordEmptyRepeat(true)
+    }
+  }
+
+  useEffect(() => {
+    if (emailError || passError || register.password !== register.password2) {
+      setValidForm(false)
+      setRepeatPass('Пароль не совподает')
+    } else {
+      setValidForm(true)
+      setRepeatPass('')
+    }
+  }, [register.email, register.password, register.password2])
 
   return (
     <form className={classes.form}>
       <div className={classes.formBlock}>
         <p>Почта</p>
-        <input type="text" />
+        <input
+          onBlur={(e) => blurHandler(e)}
+          onChange={(e) => changeInputRegister(e)}
+          type="text"
+          name="email"
+          value={register.email}
+        />
+        {emailEmpty ? <div>{emailError}</div> : ''}
       </div>
       <div className={classes.formBlock}>
         <p>Пароль</p>
-        <input type="text" />
+        <input
+          onBlur={(e) => blurHandler(e)}
+          onChange={changeInputRegister}
+          type="password"
+          name="password"
+          value={register.password}
+        />
+        {passwordEmpty ? <div>{passError}</div> : ''}
       </div>
       <div className={classes.formBlock}>
         <p>Повторите пароль</p>
-        <input type="text" />
+        <input
+          onBlur={(e) => blurHandler(e)}
+          onChange={changeInputRegister}
+          type="password"
+          name="password2"
+          value={register.password2}
+        />
+        {passwordEmptyRepeat ? <div>{repeatPass}</div> : ''}
       </div>
-      <button type="button" className={classes.saveBtn}>
+      <button disabled={!validForm} type="submit" className={classes.saveBtn}>
         Сохранить
       </button>
     </form>
