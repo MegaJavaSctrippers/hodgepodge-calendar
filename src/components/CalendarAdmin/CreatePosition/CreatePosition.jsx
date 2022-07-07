6266244279f3364428f6c65f3f45838c8d7040ee
Aigerim Erkebaeva <erkebaeva.aigerim88@gmail.com>
2022-07-07 14:52:07 +0600

    add: .idea and .vscode to .gitignore file

---
+++ src/components/CalendarAdmin/CreatePosition/CreatePosition.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function CreatePosition() {
+  return (
+    <div>
+      <div>Создать:</div>
+      <div>Должность</div>
+      <div>
+        <input type="text" placeholder="Название должности" />
+        <input type="text" />
+      </div>
+    </div>
+  )
+}
